50e7af2b2ed44140ab78886a7fc1b1e9b574aa29
wkdwlgns12 <jjg530@kbu.ac.kr>
2025-08-18 17:24:57 +0900

    init

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,6 @@ import Edit from './pages/Edit'
 import Home from './pages/Home'
 import Notfound from './pages/Notfound'
 import New from './pages/New'
-import { getEmotionImage } from './util/getEmotionImage'
 import Header from './components/Header'
 import Button from './components/Button'
 
