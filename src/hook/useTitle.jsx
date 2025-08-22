501daa6b3600fa5c8116da82b5b8de531ad19995
wkdwlgns12 <jjg530@kbu.ac.kr>
2025-08-22 15:26:40 +0900

    1

--- src/hook/useTitle.jsx
+++ src/hook/useTitle.jsx
@@ -1,12 +1,13 @@
-import React, { useEffect } from 'react'
+// src/hook/useTitle.jsx
+import { useEffect } from 'react'
 
 const useTitle = (title) => {
     useEffect(() => {
-
-        const $title = document.getElementsByTagName("title")[0]
-        $title.innerHTML = title
+        if (typeof document === 'undefined') return           // SSR 대비(혹시 모를 환경)
+        const prev = document.title                           // 이전 제목 백업
+        document.title = title || prev                        // title 없으면 이전값 유지
+        return () => { document.title = prev }                // 언마운트 시 복구
     }, [title])
-
 }
 
 export default useTitle
